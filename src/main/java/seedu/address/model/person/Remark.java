2e6f9d392e0e7c4e7871eac480a19e4c7e52a039
Nahoyhp <phyohan@hotmail.com>
2020-09-29 21:47:58 +0800

    Attempt for CI Pass Ver.3

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -1,7 +1,6 @@
 package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a Person's remark in the address book.
@@ -10,6 +9,10 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Remark {
     public final String value;
 
+    /**
+     * Initialise a remark
+     * @param remark Description of the remark
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
